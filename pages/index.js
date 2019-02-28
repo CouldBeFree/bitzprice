359831a51a04eed74b414e523b01a16a3d61597b
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-02-28 14:32:45 +0200

    Changing currency

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
+import Prices from '../components/Prices';
 
-const Index = () => (
+const Index = (props) => (
     <div>
         <Layout>
             <h1>Welcome to BitzPrice</h1>
+            <Prices bpi={props.bpi}/>
         </Layout>
     </div>
 );
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+
+    return {
+        bpi: data.bpi
+    }
+};
 
 export default Index;
