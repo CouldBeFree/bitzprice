d2c1bf08643aafcace7fa5f47ee82da139583f62
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-02-28 13:58:51 +0200

    Added bootstrap/styled navbar

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
 
 const About = () => (
     <div>
-        <Navbar/>
-        <h1>About BitzPrice</h1>
-        <p>Application to view Bitcoin prices</p>
+        <Layout>
+            <h1>About BitzPrice</h1>
+            <p>Application to view Bitcoin prices</p>
+        </Layout>
     </div>
 );
 
